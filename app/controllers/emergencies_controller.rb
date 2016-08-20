32f47583e37deb8e4bf59a48372d497578e80f67
Sanjiv <sanjiv@joshsoftware.com>
2016-08-20 14:21:45 +0530

    Fixed rubocop issue

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -5,17 +5,18 @@ class EmergenciesController < ApplicationController
 
   def create
     emergency = Emergency.create(create_params)
-    res_obj = if emergency.errors.blank?
-      {message: nil, emergency: emergency}
+    res_obj =
+    if emergency.errors.blank?
+      { message: nil, emergency: emergency }
     else
-      {status: 422, message: emergency.errors.messages}
+      { status: 422, message: emergency.errors.messages }
     end
     render json: res_obj
   end
 
   private
-    def create_params
-      params.require(:emergency).permit(:code, :medical_severity, :fire_severity, :police_severity)
-    end
 
+  def create_params
+    params.require(:emergency).permit(:code, :medical_severity, :fire_severity, :police_severity)
+  end
 end
