d42bd7b8c869e7f12930f7bc2cbe132d2c228bab
Sanjiv <sanjiv@joshsoftware.com>
2016-08-20 14:17:49 +0530

    Added create action for emergencies controller

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -2,4 +2,20 @@ class EmergenciesController < ApplicationController
   def index
     render json: { emergencies: Emergency.all }
   end
+
+  def create
+    emergency = Emergency.create(create_params)
+    res_obj = if emergency.errors.blank?
+      {message: nil, emergency: emergency}
+    else
+      {status: 422, message: emergency.errors.messages}
+    end
+    render json: res_obj
+  end
+
+  private
+    def create_params
+      params.require(:emergency).permit(:code, :medical_severity, :fire_severity, :police_severity)
+    end
+
 end
