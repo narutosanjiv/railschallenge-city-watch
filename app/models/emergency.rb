4ffecb9157284ff5835a7db26d7a7e0fa306582d
Sanjiv <sanjiv@joshsoftware.com>
2016-08-20 14:45:06 +0530

    Added presence validation

--- app/models/emergency.rb
+++ app/models/emergency.rb
@@ -1,5 +1,5 @@
 class Emergency < ActiveRecord::Base
   validates :police_severity, :medical_severity, :fire_severity, presence: true,
             numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :code, uniqueness: true
+  validates :code, uniqueness: true, presence: true
 end
